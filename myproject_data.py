92a16e784837911efbffdfad17794a02aa75b6f4
pasco78 <69111880+pasco78@users.noreply.github.com>
2020-08-20 21:28:30 +0900

    2020820
    mypoject_data가 엑셀 정리한것입니다.
    나머지는 연습 파일 입니다.
    피봇해서 모델별로 그림그려야 합니다.
    모델명 열데이터에서 숫자 2글만 추출하고  그룹 by해서 그래프를 그려야합니다. 목표 그래프 형상은 따로 올려 놓겠습니다.

--- myproject_data.py
+++ myproject_data.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pandas as pd
-#
+import re
+
+
 fruit = pd.read_excel('fruit.xlsx',sheet_name='sheet1')
 
 condition_A=(fruit['Spec.']=='전 : 0˚↓, 후 : 1.8˚ ↑(MA)')
@@ -19,7 +22,27 @@ print(apple.head())
 
 friut = pd.merge(fruit, apple, how='left', on=['모델','Suffix'])
 
-print(friut)
+friut['검사 시간'] = pd.to_datetime(friut['검사 시간'])
+friut['년월'] = friut['검사 시간'].apply(lambda x: x.strftime('%Y%m'))
+friut['검사 시간'] = friut['검사 시간'].apply(lambda x: x.strftime('%m%d'))
 
-friut.to_excel("output_merge.xlsx", index=False, encoding='utf-8')
+friut = pd.DataFrame(friut)
+friut.to_excel("output_merge.xlsx", index=True, encoding='utf-8')
+
+print(friut['모델'])
+
+friut = pd.DataFrame(friut)
+friut_table=pd.pivot_table(friut, index=['모델','Stand P/N','Stand (Type)'], columns='검사 시간', values='Value',aggfunc='first')
+
+# friut_table=friut_table.groupby(['모델','Stand P/N','Stand (Type)'])
+
+print(friut_table)
+
+friut_table.to_excel("output_table.xlsx", index=True, encoding='utf-8')
+
+
+
+
+
+
 
