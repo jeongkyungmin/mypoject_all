897109361261268e2f0487c9d72fc8c4a3f3a194
pasco78 <69111880+pasco78@users.noreply.github.com>
2020-08-22 13:29:21 +0900

    20200822

--- myproject_data.py
+++ myproject_data.py
@@ -26,12 +26,9 @@ friut['검사 시간'] = pd.to_datetime(friut['검사 시간'])
 friut['년월'] = friut['검사 시간'].apply(lambda x: x.strftime('%Y%m'))
 friut['검사 시간'] = friut['검사 시간'].apply(lambda x: x.strftime('%m%d'))
 
+friut=friut.dropna(axis=0)
+
 friut.to_excel("output_merge.xlsx", index=True, encoding='utf-8')
-
-
-print(friut['모델'])
-
-
 
 #index() 추가 연습
 
@@ -47,7 +44,43 @@ fruit_OLED = fruit.loc[fruit.모델.str.contains("OLED")]
 
 
 print(fruit_OLED)
+#
+# fruit_UHD=fruit.drop[fruit_OLED]
+#
+# print(fruit_UHD)
+
 #OLED 모델 축출 연습
+
+#ok/ng 판정하기
+#
+# grades = []
+#
+# for row in fruit['Value']:
+#     if float(row) < 1.4:
+#         grades.append('ok')
+#
+#     else:
+#         grades.append('ng')
+#
+# fruit['result'] = grades
+#
+# print(fruit)
+
+#ok/ng 판정하기
+#
+# Group by 해보기
+
+fruit_모델 = fruit.groupby('모델')
+
+print(fruit_모델.groups)
+
+# for Stand P/N, group in fruit_모델:
+#     print(Stand P/N) + ": " + str(len(group)))
+#     print(group)
+#     print()
+
+# Group by 해보기
+
 
 friut_table=pd.pivot_table(friut, index=['모델','Stand P/N','Stand (Type)'], columns='검사 시간', values='Value',aggfunc='first')
 
