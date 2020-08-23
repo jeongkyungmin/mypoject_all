47a044613b6bb8b82f44c86a211e209201bb05ca
pasco78 <69111880+pasco78@users.noreply.github.com>
2020-08-23 22:58:40 +0900

    20200823_3rd
    OLED/UHD 모델 구분을 하고 싶습니다.

--- myproject_data.py
+++ myproject_data.py
@@ -2,16 +2,14 @@ import numpy as np
 import pandas as pd
 import re
 
-
 fruit = pd.read_excel('fruit.xlsx',sheet_name='sheet1')
-# fruit=fruit.rename (index = { 'Spec.': 'Spec'}, inplace = True)
 
 condition_A=(fruit['Spec.']=='전 : 0˚↓, 후 : 1.8˚ ↑(MA)')
 fruit=fruit[condition_A]
 
 fruit = fruit[['검사 시간','모델', 'Suffix','라인','W/O','Spec.','Value']]
 
-fruit = fruit.rename({'Spec.':'Spec'},axis='columns')
+fruit = fruit.rename({'Spec.':'Spec','모델':'Model'},axis='columns')
 
 fruit=fruit.dropna()
 
@@ -20,12 +18,12 @@ print("필요한 컬럼 추출과과 빈셀 삭제!!" + fruit)
 apple = pd.read_excel('apple.xlsx',sheet_name='sheet1')
 
 apple = apple[['모델','Suffix','Stand P/N','Stand (Type)']]
-apple = apple.rename({'Stand P/N':'Stand No','Stand (Type)':'Stand'},axis='columns')
+apple = apple.rename({'Stand P/N':'Stand No','Stand (Type)':'Stand','모델':'Model'},axis='columns')
 
 
 print("apple 종류는 이런것들이 있어요!"+ apple)
 
-fruit = pd.merge(fruit, apple, how='left', on=['모델','Suffix'])
+fruit = pd.merge(fruit, apple, how='left', on=['Model','Suffix'])
 
 print("fruit과 apple을 merge했어요!..그래야 fruit와 apple을 이용해서 Value값의 구분자로 쓸수 있어요!!" + fruit)
 
@@ -42,19 +40,30 @@ fruit.to_excel("output_merge.xlsx", index=True, encoding='utf-8')
 
 print(fruit)
 
-##OLED/UHD 모델 추출 연습
-fruit_OLED = fruit.loc[fruit.모델.str.contains("OLED")]
+#OLED/UHD 모델 추출 연습
+fruit_OLED = fruit.loc[fruit['Model'].str.contains('OLED')]
 
 print(fruit_OLED)
 
-# fruit_UHD = fruit.drop[fruit.모델.str.contains("OLED")]
+print(fruit['Model'].str.contains('OLED'))
 
-# print(fruit_UHD)
+a = []
+for row in fruit['Model']:
+    if fruit['Model'].str.contains('OLED').Models[row] =='False':
+        a.append('OLED')
+
+    else:
+        a.append('UHD')
+
+fruit['제품군'] = a
+
+print(fruit)
+
 
 ##OLED/UHD 모델 추출 연습
-#
+
 ## ok/ng 판정하기
-#
+
 print(fruit['Value'].count())
 print(type(float(fruit['Value'].values[0])))
 
@@ -70,27 +79,22 @@ fruit['result'] = result
 print(fruit)
 
 fruit.to_excel("result.xlsx", index=True, encoding='utf-8')
-#
+
 # # #ok/ng 판정하기
-#
-fruit_table=pd.pivot_table(fruit, index=['모델','Stand','Stand No'], columns='index', values='Value',aggfunc='first')
+
+fruit_table=pd.pivot_table(fruit, index=['Model','Stand','Stand No'], columns='index', values='Value',aggfunc='first')
 
 fruit_table.to_excel("output_table.xlsx", index=True, encoding='utf-8')
 print(fruit_table)
 
 ## Group by 해보기 :이건 참고 자료 입니다!!
-# fruit_table.groupby('모델')
-print(fruit_table.groupby('모델').get_group('OLED77CXPVA'))
-print(fruit_table.groupby('모델').size())
-fruit_table_모델_cnt = pd.DataFrame({'count' : fruit_table.groupby('모델').size()}).reset_index()
-print(fruit_table_모델_cnt)
-#
-# fruit_table.groupby('Stand')
+
+print(fruit_table.groupby('Model').get_group('OLED77CXPVA'))
+print(fruit_table.groupby('Model').size())
+fruit_table_Model_cnt = pd.DataFrame({'count' : fruit_table.groupby('Model').size()}).reset_index()
+print(fruit_table_Model_cnt)
+
 fruit_table_Stand_cnt = pd.DataFrame({'count' : fruit_table.groupby('Stand').size()}).reset_index()
 print(fruit_table_Stand_cnt)
 ## Group by 해보기 :이건 참고 자료 입니다!!
-
-
-
-
 
